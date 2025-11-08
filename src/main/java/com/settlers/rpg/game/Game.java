20312f065860613cb1913c6bccd8784b6c2b0387
Jerry Juinior Pacalso <pacalsojerry@gmail.com>
2025-11-08 10:29:46 +0800

    Create Game.java that runs the game loop

---
+++ src/main/java/com/settlers/rpg/game/Game.java
@@ -0,0 +1,8 @@
+package com.settlers.rpg.game;
+
+public class Game {
+    public void start(){
+        System.out.println("Starting game loop");
+        System.out.println("Game loop ends");
+    }
+}
