36698479968399032a8f66cc2d2a302563b4e935
Jerry Juinior Pacalso <pacalsojerry@gmail.com>
2025-11-08 10:32:30 +0800

    Add TODO for GameLoop class in Game.java

--- src/main/java/com/settlers/rpg/game/Game.java
+++ src/main/java/com/settlers/rpg/game/Game.java
@@ -3,6 +3,7 @@ package com.settlers.rpg.game;
 public class Game {
     public void start(){
         System.out.println("Starting game loop");
+        //TODO: Create GameLoop.java and run the loop here
         System.out.println("Game loop ends");
     }
 }
