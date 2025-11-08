8fec7e5824d138d3434e1076189eb4714ec0d2a0
Jerry Juinior Pacalso <pacalsojerry@gmail.com>
2025-11-08 21:13:01 +0800

    Implement actual game loop logic

--- app/src/main/java/com/settlers/rpg/scenes/Scene.java
+++ app/src/main/java/com/settlers/rpg/scenes/Scene.java
@@ -2,7 +2,7 @@ package com.settlers.rpg.scenes;
 
 public interface Scene {
     void render();
-    void handleInput(String input);
-    void update();
+    Scene handleInput(String input);
+    Scene update();
     String getName();
 }
