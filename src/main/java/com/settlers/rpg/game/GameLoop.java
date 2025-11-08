a8956e9c8680befe3776c42ba909829014ccf369
Jerry Juinior Pacalso <pacalsojerry@gmail.com>
2025-11-08 10:41:10 +0800

    Create GameLoop.java

---
+++ src/main/java/com/settlers/rpg/game/GameLoop.java
@@ -0,0 +1,8 @@
+package com.settlers.rpg.game;
+
+public class GameLoop {
+    public void run() {
+        System.out.println("class=GameLoop, method=run, message=Start Game Loop");
+        System.out.println("class=GameLoop, method=run, message=End Game Loop");
+    }
+}
