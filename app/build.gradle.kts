f492108f4d44ebe7808d70397c521637ebe2f96b
JerryJuniorPacalso <43496853+JerryJuniorPacalso@users.noreply.github.com>
2025-11-15 16:57:50 +0800

    Create checks for test coverage make sure pull requests are atleast 90 covered (#71)
    * Update gradle build to generate jacoco files (#69)
    
    * Update application gradle file to include generation of jacoco report
    
    * Rewrite test task cleanly
    
    * Explicitly add the java plugin in gradle build file
    
    * Add workflow to check test coverage (#70)
    
    * Guard qodana refs to check for pr or push refs (#73)
    
    * Add guard to use pr or push ref
    
    * Fix format
    
    * Update codeql-action upload to v4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,17 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
+    java
     application
+    jacoco
+    id("jvm-test-suite")
 }
 
 group = "com.settlers"
 version = "0.0.1"
+
+jacoco {
+    toolVersion = "0.8.12"
+}
 
 repositories {
     // Use Maven Central for resolving dependencies.
@@ -17,16 +24,10 @@ dependencies {
 
     // Test dependencies
     testImplementation("org.mockito:mockito-core:5.14.0")
-}
-
-testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use JUnit Jupiter test framework
-            useJUnitJupiter("5.12.1")
-        }
-    }
+    testImplementation(platform("org.junit:junit-bom:5.12.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -43,4 +44,31 @@ application {
 
 tasks.named<JavaExec>("run") {
     standardInput = System.`in`
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // Generate report after tests
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.90".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
 }
